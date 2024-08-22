5b647b63cf7f0925aa3384c757dd704b5e68ed82
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-22 13:42:11 -0300

    feat: criando os cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { ContainerComponent } from './shared/container/container.component';
 import { HomeComponent } from './shared/pages/home/home.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -21,8 +23,15 @@ import { HomeComponent } from './shared/pages/home/home.component';
     CardComponent,
     ContainerComponent,
     HomeComponent,
+    CardBuscaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
