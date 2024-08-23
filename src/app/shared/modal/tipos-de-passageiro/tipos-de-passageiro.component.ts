e0ccb15408cade7ae4935f144bf66c3bb750856d
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-23 12:11:18 -0300

    feat: tornando dinamico

--- src/app/shared/modal/tipos-de-passageiro/tipos-de-passageiro.component.ts
+++ src/app/shared/modal/tipos-de-passageiro/tipos-de-passageiro.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tipos-de-passageiro',
   templateUrl: './tipos-de-passageiro.component.html',
-  styleUrls: ['./tipos-de-passageiro.component.scss']
+  styleUrls: ['./tipos-de-passageiro.component.scss'],
 })
 export class TiposDePassageiroComponent {
-
+  @Input() tipoIdade: string = '';
+  @Input() periodoIdade: string = '';
 }
